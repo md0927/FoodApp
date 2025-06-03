efd1bcd43cd83a0940e5fe5141f2834d4223b2ab
Bhavanisimhadri <bhavani.simhadri1027@gmail.com>
2025-06-03 10:47:31 +0530

    update

--- src/Screens/Navigation/AppNavigator.js
+++ src/Screens/Navigation/AppNavigator.js
@@ -4,6 +4,7 @@ import HomePage from '../HomePage';
 import LoginPage from '../Loginpage';
 import CartScreen from '../CartScreen';
 import SearchResultsScreen from '../SearchResultsScreen';
+import Register from '../Register';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ const AppNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="Login">
       <Stack.Screen name="Login" component={LoginPage} options={{headerShown:false}}/>
+      <Stack.Screen name="Register" component={Register} options={{headerShown:false}}/>
      <Stack.Screen name="Home" component={HomePage} options={{headerShown:false}}/>
       <Stack.Screen name="Cart" component={CartScreen} options={{headerShown:false}} />
       <Stack.Screen name="SearchResults" component={SearchResultsScreen} options={{headerShown:false}} />
